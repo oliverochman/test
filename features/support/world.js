ad2c94e41ec9fd760e7bdfdeac3ce3fb6f868ae3
oliverochman <oliver.ochman@hotmail.com>
2018-10-31 13:51:17 +0100

    render contacts on page

--- features/support/world.js
+++ features/support/world.js
@@ -45,6 +45,13 @@ class AddressBookWorld {
     expect(actualCount).to.be.eq(expectedCount)
   }
 
+  async pageDoesNotHaveTextContent(unexpectedContent) {
+    const pageContent = await this.page.content()
+    let actualContent = pageContent.match(unexpectedContent)
+
+    expect(actualContent).to.be.eq(null)
+  }
+
   btnSelectorFromName(btnName) {
     switch (btnName) {
       case 'add contact':
