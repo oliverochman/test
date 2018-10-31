49930e41fe0ffe21bfb2519e427f0dbf44f2812e
oliverochman <oliver.ochman@hotmail.com>
2018-10-31 11:29:33 +0100

    add contacts header

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,13 +1,11 @@
-const { Given, Then, When } = require('cucumber')
+const { Given, Then, When, After } = require('cucumber')
 
 Given('I visit the site', async function() {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  return await this.openHomePage();
 });
 
 Then('I should see {string}', async function(content) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  return await this.pageHasTextContent(content);
 })
 
 When('I click {string}', async function(string) {
@@ -28,4 +26,8 @@ Then('I should have {int} contact in my address book', async function(int) {
 Then('I should not see {string}', async function(string) {
   // Write code here that turns the phrase above into concrete actions
   return 'pending'
+})
+
+After(async function() {
+  return await this.closeHomePage();
 })
